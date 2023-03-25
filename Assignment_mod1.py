f72f65bc10d7275ff806a78021526727c83e73cc
Abhishek2229 <125631909+Abhishek2229@users.noreply.github.com>
2023-03-25 12:32:21 +0530

    Add files via upload

---
+++ Assignment_mod1.py
@@ -0,0 +1,20 @@
+1.What is software?
+# Software is a set of instructions, data or programsused to operate computers and execute specific tasks.
+
+2. What are the types of applications?
+# 1. Web development
+  2. Machine learning and artificial intelligence
+  3. Data science
+  4. Game development.
+  5. Audio and visual applications.
+  6. Software development.
+  7. CAD applications.
+  8. Business applications.
+  9. Desktop GUI
+  10. Web scraping application.
+
+3. What is programming?
+# Programming is the process or activity of writing computer programs.
+
+4. What is Python?
+# Ppython is a computer programming language often used to build website and software, automamte tasks and conduct data analysis.
